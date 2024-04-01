6eaf3df571849855ca41b3251d9646393887dcf0
maldikurniawan <akurniawan2102@gmail.com>
2024-04-01 23:09:12 +0700

    add about

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import Nav from './Components/Nav'
+import Banner from './Components/Banner'
+import About from './Components/About'
 
 function App() {
   return (
     <h1 className="bg-slate-900">
       <Nav />
+      <Banner />
+      <About />
     </h1>
   )
 }
